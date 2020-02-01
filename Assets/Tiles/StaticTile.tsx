5c9d2c2d480a44d7f854bac5b3e73faa8f92e3e5
Marzio Cuello <marzio.cuello@gmail.com>
2020-02-01 17:41:46 -0300

    Tiles v1

---
+++ Assets/Tiles/StaticTile.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.2" name="StaticTile" tilewidth="46" tileheight="46" tilecount="1" columns="1">
+ <image source="Tiles/StaticTile.png" width="46" height="46"/>
+</tileset>
