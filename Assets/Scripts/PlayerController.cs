da7b3ef9e876109f9a312d97e4802bfcf0926430
Santiago Pacheco <pachesantiago@gmail.com>
2020-02-01 11:52:02 -0300

    Added basic character movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : KinematicObject
+{
+    public enum JumpState
+    {
+        Grounded,
+        PrepareToJump,
+        Jumping,
+        InFlight,
+        Landed
+    }
+
+    // Public interface
+    public float moveSpeed = 7;
+    public float jumpTakeOffSpeed = 7;
+
+    // Internal state
+    private Collider2D collider2d;
+    private SpriteRenderer spriteRenderer;
+    private Animator animator;
+    private Vector2 movementVec;
+    private JumpState jumpState = JumpState.Grounded;
+
+    void Awake()
+    {
+        collider2d = GetComponent<Collider2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+    }
+
+    protected override void Update()
+    {
+        movementVec.x = Input.GetAxis("Horizontal");
+
+        if (jumpState == JumpState.Grounded && Input.GetButtonDown("Jump"))
+            jumpState = JumpState.PrepareToJump;
+
+        switch (jumpState)
+        {
+            case JumpState.PrepareToJump:
+                break;
+            case JumpState.Jumping:
+                if (!IsGrounded)
+                {
+                    //Schedule<PlayerJumped>().player = this;
+                    jumpState = JumpState.InFlight;
+                }
+                break;
+            case JumpState.InFlight:
+                if (IsGrounded)
+                {
+                    //Schedule<PlayerLanded>().player = this;
+                    jumpState = JumpState.Landed;
+                }
+                break;
+            case JumpState.Landed:
+                jumpState = JumpState.Grounded;
+                break;
+        }
+
+        base.Update();
+    }
+
+    protected override void ComputeVelocity()
+    {
+        if (jumpState == JumpState.PrepareToJump && IsGrounded)
+        {
+            velocity.y = jumpTakeOffSpeed;
+            jumpState = JumpState.Jumping;
+        }
+
+        if (movementVec.x > 0.01f)
+            spriteRenderer.flipX = false;
+        else if (movementVec.x < -0.01f)
+            spriteRenderer.flipX = true;
+
+        animator.SetBool("grounded", IsGrounded);
+        animator.SetFloat("velocityX", Mathf.Abs(velocity.x) / moveSpeed);
+
+        targetVelocity = movementVec * moveSpeed;
+    }
+}
