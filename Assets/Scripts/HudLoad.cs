1aaa99f9e5b3c1e921a374c5a897acb8dbf1bb1f
Artigas <artigas72@gmail.com>
2020-02-01 20:43:05 -0300

    Music + Hud to Lvl1

--- Assets/Scripts/HudLoad.cs
+++ Assets/Scripts/HudLoad.cs
@@ -5,9 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class HudLoad : MonoBehaviour
 {
-    public int HUD;
     void Start()
     {
-        SceneManager.LoadScene(HUD, LoadSceneMode.Additive);
+        SceneManager.LoadScene("Hud", LoadSceneMode.Additive);
     }
 }
