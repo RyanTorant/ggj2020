c08543f3ea9b44eaee431f4a3831df363de63fef
Santiago Pacheco <pachesantiago@gmail.com>
2020-02-01 17:55:29 -0300

    Added the move and idle animation

--- Assets/GrabBehaviour.cs
+++ Assets/GrabBehaviour.cs
@@ -2,15 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MovingBehaviour : StateMachineBehaviour
+public class GrabBehaviour : StateMachineBehaviour
 {
-    public float MoveFadeOut = 0.5f;
-    public float MoveFadeIn = 0.1f;
-
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        AudioManager.PlayMoving(MoveFadeIn);
+        AudioManager.PlaySound(SFX.Grab);
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
@@ -20,10 +17,10 @@ public class MovingBehaviour : StateMachineBehaviour
     //}
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
-    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    {
-        AudioManager.StopMoving(MoveFadeOut);  
-    }
+    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
 
     // OnStateMove is called right after Animator.OnAnimatorMove()
     //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
--- Assets/JumpStartBehaviour.cs
+++ Assets/JumpStartBehaviour.cs
@@ -7,7 +7,7 @@ public class JumpStartBehaviour : StateMachineBehaviour
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        AudioManager.PlaySound(AudioManager.JumpSFX);
+        AudioManager.PlaySound(SFX.StartJump);
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
@@ -19,7 +19,7 @@ public class JumpStartBehaviour : StateMachineBehaviour
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        
+        AudioManager.PlaySound(SFX.EndJump);
     }
 
     // OnStateMove is called right after Animator.OnAnimatorMove()
