1a8c2bfd5dc1f24b73cbbd1c1022c4a1fad1ba1d
Santiago Pacheco <pachesantiago@gmail.com>
2020-02-02 10:13:02 -0300

    Added grabbed flag into tile

---
+++ Assets/DynamicTile.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DynamicTile : MonoBehaviour
+{
+    public bool IsBeingGrabbed = false;
+    private Rigidbody2D body;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var vel = Physics2D.gravity * Time.deltaTime;
+        //body.position = body.position + Physics2D.gravity * Time.deltaTime;
+    }
+}
