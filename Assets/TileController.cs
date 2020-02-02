c998bad294825844fd164d329a5a1609c7b4ed31
Marzio Cuello <marzio.cuello@gmail.com>
2020-02-01 22:04:31 -0300

    marzio level fixes

---
+++ Assets/TileController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileController : KinematicObject
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    protected override void ComputeVelocity()
+    {
+        targetVelocity.y = -9.5f;
+
+    }
+}
